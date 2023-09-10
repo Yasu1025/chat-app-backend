ad0a159bd1c5620c8b05d181cfbb565f6ab2d799
Yasu1025 <sillyjp25@gmail.com>
2023-09-10 17:22:45 +0900

    [Feature] securityMiddleware

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -1,10 +1,11 @@
-import { Application } from "express";
+import { Application, json, urlencoded } from "express";
 import http from "http";
 import cors from "cors";
 import helmet from "helmet";
 import hpp from "hpp";
 import cookieSession from "cookie-session";
 import HTTP_STATUS from "http-status-codes";
+import compression from "compression";
 import "express-async-errors";
 
 export class AppServer {
@@ -22,8 +23,35 @@ export class AppServer {
     this.standardMiddleware(this.app);
   }
 
-  private securityMiddleware(app: Application): void {}
-  private standardMiddleware(app: Application): void {}
+  private securityMiddleware(app: Application): void {
+    app.use(
+      cookieSession({
+        name: "session",
+        keys: ["test1", "test2"], // TODO: change later
+        maxAge: 24 * 7 * 3600000, // 7 days
+        secure: false, // TODO: change later
+      })
+    );
+
+    app.use(hpp()); // For http param pollutions
+    app.use(helmet()); // Security header
+    app.use(
+      cors({
+        origin: "*", // TODO: change later
+        credentials: true, // caz we use cookie
+        optionsSuccessStatus: 200,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      })
+    );
+  }
+
+  private standardMiddleware(app: Application): void {
+    const limitMB = "50mb";
+    app.use(compression()); // to help compress our req
+    app.use(json({ limit: limitMB }));
+    app.use(urlencoded({ extended: true, limit: limitMB }));
+  }
+
   private routeMiddleware(app: Application): void {}
   private globalErrorHandler(app: Application): void {}
   private startServer(app: Application): void {}
