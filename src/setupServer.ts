fa93c71f087b5e10e26169f5d156f79466759508
Yasu1025 <sillyjp25@gmail.com>
2023-09-15 22:04:25 +0900

    [Feature] setup SocketIO

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -8,6 +8,10 @@ import HTTP_STATUS from "http-status-codes";
 import compression from "compression";
 import "express-async-errors";
 import { config } from "./config";
+import { Server } from "socket.io";
+import { createClient } from "redis";
+import { connect } from "mongoose";
+import { createAdapter } from "@socket.io/redis-adapter";
 
 const SERVER_PORT = 6000;
 
@@ -65,18 +69,34 @@ export class AppServer {
   private async startServer(app: Application): Promise<void> {
     try {
       const httpServer: http.Server = new http.Server(app);
+      const socketIO: Server = await this.createSocketIO(httpServer);
       this.startHttpServer(httpServer);
+      this.socketIOConnection(socketIO);
     } catch (error) {
       console.log(error);
     }
   }
 
-  // TODO
-  private createSocketIO(httpServer: http.Server): void {}
+  private async createSocketIO(httpServer: http.Server): Promise<Server> {
+    const io: Server = new Server(httpServer, {
+      cors: {
+        origin: config.CLIENT_URL,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      },
+    });
+    const pubClient = createClient({ url: config.REDIS_HOST });
+    const subClient = pubClient.duplicate();
+    await Promise.all([pubClient.connect(), subClient.connect()]);
+    io.adapter(createAdapter(pubClient, subClient));
+    return io;
+  }
 
   private startHttpServer(httpServer: http.Server): void {
+    console.log(`Server has started with process ${process.pid}`);
     httpServer.listen(SERVER_PORT, () => {
       console.log(`Server running on port ${SERVER_PORT}`);
     });
   }
+
+  private socketIOConnection(io: Server): void {}
 }
